e7b7a4ecf37893e86c60fde55cc9d1ead116ffe3
wiissal <wissaloa1@gmail.com>
2026-04-26 11:32:56 +0100

    chore: add database  config and env example

---
+++ backend/src/config/database.js
@@ -0,0 +1,20 @@
+const {pool} = require ('pg');
+require('dotenv').config();
+
+const pool = new pool|({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+pool.on ('connect', (err)=>{
+  console.log('Connected to the database');
+});
+
+pool.on ('error', (err)=>{
+ console.error('postgreSQL error:', err);
+ process.exit(-1);
+})
+module.exports = pool;
